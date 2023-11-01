f9f0fc74674f6cf16cf6c562fe9eaf8df52674bb
Kartik2057 <2021kuec2057@iiitkota.ac.in>
2023-11-02 03:27:09 +0530

    Modified app structure

--- src/App.js
+++ src/App.js
@@ -3,18 +3,28 @@ import './App.css';
 import Navigation from './customer/components/navigation/Navigation';
 import Homepage from './customer/pages/Homepage';
 import Footer from './customer/components/footer/Footer';
-import Product from './customer/pages/ProductPage';
+import ProductPage from './customer/pages/ProductPage';
+import ProductDetailPage from './customer/pages/ProductDetailPage';
+import CartPage from './customer/pages/CartPage';
+import CheckoutPage from './customer/pages/CheckoutPage';
+import OrdersPage from './customer/pages/OrdersPage';
+import OrderDetailPage from './customer/pages/OrderDetailPage';
 
 function App() {
   return (
     <div className="">
-      <Navigation/>
-      <div>
-        {/* <Homepage/> */}
-        <Product/>
+        <Navigation/>
+        <div>
+          {/* <Homepage/> */}
+          {/* <ProductPage/> */}
+          {/* <ProductDetailPage/> */}
+          {/* <CartPage/> */}
+          {/* <CheckoutPage/> */}
+          {/* <OrdersPage/> */}
+          <OrderDetailPage/>
+        </div>
+        {/* <Footer/> */}
       </div>
-      <Footer/>
-    </div>
   );
 }
 
--- src/customer/pages/ProductPage.js
+++ src/customer/pages/ProductPage.js
@@ -9,7 +9,6 @@ import Radio from '@mui/material/Radio';
 import RadioGroup from '@mui/material/RadioGroup';
 import FormControlLabel from '@mui/material/FormControlLabel';
 import FormControl from '@mui/material/FormControl';
-import FormLabel from '@mui/material/FormLabel';
 import FilterListIcon from '@mui/icons-material/FilterList';
 import { useLocation, useNavigate } from 'react-router-dom';
 
